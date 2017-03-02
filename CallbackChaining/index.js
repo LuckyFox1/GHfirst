b467314015b7adcdf4b23bedde7ccfaa5c4e5179
rizorko <rizorko@yandex.ru>
2017-03-02 10:27:29 +0200

    Callback chaining

---
+++ CallbackChaining/index.js
@@ -0,0 +1,36 @@
+function perform(nonce, cb) {
+	//TODO implement
+	var q = [];
+	
+	var next = function(param) {
+		var callback = q.shift();
+		if (callback) {
+			var ret = callback(param);
+			next(ret);
+		}
+	}
+
+	var then = function(cb) {
+		q.push(cb);
+		return { then: then }
+	}
+
+	var firstParam = cb();
+	setTimeout(() => { next(firstParam); }, 0);
+
+	return { then: then };
+}
+
+perform(null, function() {
+	var param = 1;
+	console.log(param);
+	return param;
+})
+.then(function(param) {
+	console.log(++param);
+	return param;
+})
+.then(function(param) {
+	console.log(++param);
+	return param;
+});
